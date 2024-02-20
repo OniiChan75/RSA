4674939fe700e4cfe3bf8af625ffa490eb1d7e8a
SuzuKanae <melesgabriele@gmail.com>
2024-02-20 12:44:30 +0100

    modify readme.md and EuclideEsteso

--- EuclideEsteso.py
+++ EuclideEsteso.py
@@ -1,12 +1,12 @@
-def euclide_esteso(a,b):
-    x0, y0 = 1, 0
-    x1, y1 = 0, 1
+def euclide_esteso(a, b):
+    a, b = abs(a), abs(b)
+    x0, x1, y0, y1 = 1, 0, 0, 1
     while b != 0:
-        q = a // b
-        a, b = b, a % b
+        q, a, b = a // b, b, a % b
         x0, x1 = x1, x0 - q * x1
         y0, y1 = y1, y0 - q * y1
-    return a, x0, y1
+    return a, x0, y0
+
 
 def euclide(a, b):
     r = a % b
@@ -16,19 +16,23 @@ def euclide(a, b):
         r = a % b
     return b
 
-if __name__ == '__main__':
-    # funzioane euclide
-    # a = int(input("Inserisci il primo numero: "))
-    # b = int(input("Inserisci il primo numero: "))
-    # if a > b:
-    #     print(euclide(a, b))
-    # else:
-    #     print(euclide(b, a))
 
-    # funzione euclide esteso
-    a = int(input("Inserisci il primo numero: "))
-    b = int(input("Inserisci il primo numero: "))
-    if a > b:
-        print(euclide_esteso(a, b))
-    else:
-        print(euclide_esteso(b, a))
+if __name__ == '__main__':
+    sc = int(input("1. Euclide\n"
+                   "2. Euclide esteso\n"))
+    if sc == 1:
+        # funzione euclide
+        a = int(input("Inserisci il primo numero: "))
+        b = int(input("Inserisci il primo numero: "))
+        if a > b:
+            print(euclide(a, b))
+        else:
+            print(euclide(b, a))
+    elif sc == 2:
+        # funzione euclide esteso
+        a = int(input("Inserisci il primo numero: "))
+        b = int(input("Inserisci il primo numero: "))
+        if a > b:
+            print(euclide_esteso(a, b))
+        else:
+            print(euclide_esteso(b, a))
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-RSA FILE TEST
+## RSA File Test
+This is a test file for RSA encryption and decryption.
+
+## Contents:
+- `RSA.py`: File that performs the calculation of b^e mod n
+- `EuclideEsteso.py`: File that performs the normal and extended Euclidean algorithm
+
+## How to run:
+To run the program, simply run either file with python3.12 or higher.
